949fa97ad01dbfd3749d96c8a48140e912010f62
Swapnadeep Mohapatra <swapnadeepmohapatra@yahoo.com>
2020-04-15 18:27:14 +0530

    added feat to show all contacts

--- app/src/main/java/com/swapnadeep/videocallingapp/MainActivity.java
+++ app/src/main/java/com/swapnadeep/videocallingapp/MainActivity.java
@@ -2,12 +2,25 @@ package com.swapnadeep.videocallingapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
     private BottomNavigationView navView;
     private RecyclerView recyclerViewContactList;
     private ImageView findPeopleButton;
+    private DatabaseReference contactsRef;
+    private DatabaseReference userRef;
+    private String userName, profileImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +47,9 @@ public class MainActivity extends AppCompatActivity {
 
         navView = findViewById(R.id.nav_view);
         navView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+
+        contactsRef = FirebaseDatabase.getInstance().getReference().child("Contacts").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+        userRef = FirebaseDatabase.getInstance().getReference().child("Users");
 
         recyclerViewContactList = findViewById(R.id.contact_list);
         findPeopleButton = findViewById(R.id.find_people_btn);
@@ -72,5 +91,64 @@ public class MainActivity extends AppCompatActivity {
             return false;
         }
     };
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Contact> firebaseRecyclerOptions = new FirebaseRecyclerOptions.Builder<Contact>().setQuery(contactsRef.child(FirebaseAuth.getInstance().getCurrentUser().getUid()), Contact.class).build();
+
+        FirebaseRecyclerAdapter<Contact, ContactsViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Contact, ContactsViewHolder>(firebaseRecyclerOptions) {
+            @Override
+            protected void onBindViewHolder(@NonNull final ContactsViewHolder contactsViewHolder, int i, @NonNull Contact contact) {
+                final String listUserId = getRef(i).getKey();
+
+                userRef.child(listUserId).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists()) {
+                            userName = dataSnapshot.child("name").getValue().toString();
+                            profileImage = dataSnapshot.child("image").getValue().toString();
+
+                            contactsViewHolder.userNameText.setText(userName);
+                            Picasso.get().load(profileImage).into(contactsViewHolder.contactImageView);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+            }
+
+            @NonNull
+            @Override
+            public ContactsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.contact_item, parent, false);
+                ContactsViewHolder viewHolder = new ContactsViewHolder(view);
+                return viewHolder;
+            }
+        };
+
+        recyclerViewContactList.setAdapter(firebaseRecyclerAdapter);
+        firebaseRecyclerAdapter.startListening();
+
+    }
+
+    public static class ContactsViewHolder extends RecyclerView.ViewHolder {
+        TextView userNameText;
+        ImageView contactImageView;
+        Button callButton;
+
+        public ContactsViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            contactImageView = itemView.findViewById(R.id.image_contact);
+            userNameText = itemView.findViewById(R.id.name_contact);
+            callButton = itemView.findViewById(R.id.call_button);
+        }
+    }
 
 }
