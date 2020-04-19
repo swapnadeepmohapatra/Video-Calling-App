4758c4273c471215ecdeb7f6cc3b1b246e6a9a6d
Swapnadeep Mohapatra <swapnadeepmohapatra@yahoo.com>
2020-04-19 22:46:31 +0530

    added opentok depen

---
+++ app/src/main/java/com/swapnadeep/videocallingapp/VideoChattingActivity.java
@@ -0,0 +1,14 @@
+package com.swapnadeep.videocallingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VideoChattingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_chatting);
+    }
+}
